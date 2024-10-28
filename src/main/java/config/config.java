fe72cbe6dce5a5f9d673eda455597fcb0cedf083
A_Hesham <abdoe980@gmail.com>
2024-10-28 03:15:19 +0300

    Added clarifying comments for updating the 'testFolderPath' variable, detailing the location of the 'authTestData.json' file.

--- src/main/java/config/config.java
+++ src/main/java/config/config.java
@@ -3,6 +3,15 @@ package config;
 public class config {
     public static final String base_url = "https://www.saucedemo.com";
     public static final String inventory_url = "https://www.saucedemo.com/inventory.html";
+/************************************************************************************************************
+    # Need to be edited:
+        - Navigate to test_data folder under java main folder
+        - Right-Click on "authTestData.json" file
+        - Click on Copy Path/Reference
+        - Click Absolute Path
+        - Now paste it in testFolderPath without "authTestData.json"
+        - Now you are ready to go :)
+ *************************************************************************************************************/
     public static final String testFolderPath = "D:\\Digital Egypt Pioneers Initiative\\Content\\Tech\\My_Project\\My_Test\\src\\main\\java\\test_data\\";
 
     //Enum files
